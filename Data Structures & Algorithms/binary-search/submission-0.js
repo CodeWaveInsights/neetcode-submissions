d5c4056e3c6af2eaea174052e8c7a146e416d986
Ashraf Ali <ashrafalisvn@gmail.com>
2026-06-13 23:51:32 +0530

    Updated the binary function to execute locally

--- Data Structures & Algorithms/binary-search/submission-0.js
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -5,7 +5,7 @@ class Solution {
      * @return {number}
      */
     search(nums, target) {
-        //nums = [-1,0,2,4,6,8]
+        //nums = [-1,0,2,4,6,8]        winget install --id Git.Git -e --source winget        winget install --id Git.Git -e --source winget
 
         let left = 0;
         let right = nums.length-1;
@@ -26,3 +26,7 @@ class Solution {
         return -1;
     }
 }
+
+const solution = new Solution();
+const searchItem = solution.search([-1,0,2,8,1,6], 2);
+console.log(searchItem);
